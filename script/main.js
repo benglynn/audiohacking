66f4eaab0a83e22b375412ca2496c458b438ee32
Ben Glynn <ben@potatolondon.com>
2012-10-22 09:03:52 +0100

    Nicely produced samples. Loading for multiple sounds.

--- script/main.js
+++ script/main.js
@@ -7,30 +7,33 @@
 
 	var 
 	onWindowLoad,
-	context, loadAudio, playAudio,
-	audioUrl = 'audio/sound.wav', // Doesn't like the AIFF codec, convert this
-	buffer=null;
+	context, loadBuffers, onBufferLoad, playAudio,
+	names = ['bd', 'sn'],
+	buffers = [];
 
-	// Load audio
-	loadAudio = function () {
-		var request;
+	onBufferLoad = function(e) {
+		var request = e.target;
+		context.decodeAudioData(
+			request.response, function (buffer) {
+				playAudio(buffer);
+			},
+			function () {
+				console.error('Error decoding audio');
+			});
+	};
 
-		request = new XMLHttpRequest();
-		request.open('GET', audioUrl, true);
-		request.responseType = 'arraybuffer';
-		
-		request.onload = function() {
-			context.decodeAudioData(
-				request.response, function (_buffer) {
-					buffer = _buffer;
-					console.log('loaded');
-					playAudio(_buffer);
-				},
-				function () {
-					console.error('Error decoding audio');
-				});
-		};
-		request.send();
+
+	loadBuffers = function () {
+		names.forEach(function (name, i) {
+
+			var request, url = 'audio/' + name + '.mp3';
+			
+			request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.responseType = 'arraybuffer';
+			request.onload = onBufferLoad;
+			request.send();
+		});
 	};
 
 
@@ -49,7 +52,7 @@
 			console.error('Unbale to instantiate audio context');
 		}
 		if (context) {
-			loadAudio(context);
+			loadBuffers(context);
 		}
 	};
 
