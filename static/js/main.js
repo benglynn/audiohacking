db62af7ed3bd245b4d096a3076bb0921195a4e0f
Ben Glynn <ben@potatolondon.com>
2012-10-26 16:18:32 +0100

    Removed jasmine test

--- static/js/main.js
+++ static/js/main.js
@@ -12,21 +12,11 @@ requirejs.config({
 
 requirejs(['app/sample', 'app/manifest'], function (sample, manifest) {
 
-	var mp3s = {}, plusone;
+	var mp3s = {};
 
 	// Iterate over manifest and instantiate mp3s
 	manifest.mp3.forEach(function (mp3) {
 		mp3s[mp3.name] = sample.create(mp3.name, mp3.url, mp3.bytes);
 	});
 
-	// A simple function to get jasmine up and running
-	plusone = function (num) {
-		return num + 1;
-	};
-
-	// Todo: implement requirejs in Node
-	if(typeof exports !== 'undefined') {
-		// Export for node.js
-		exports.plusone = plusone;
-	}
 });
